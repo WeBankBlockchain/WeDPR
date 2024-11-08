f183fa1fd14a100ebba956579e54eacf9b036550
caryliao(廖飞强) <caryliao@webank.com>
2024-11-08 17:00:11 +0800

    remove wedpr admin

--- wedpr-components/admin/src/main/java/com/webank/wedpr/components/admin/request/GetDatasetDateLineRequest.java
+++
@@ -1,9 +0,0 @@
-package com.webank.wedpr.components.admin.request;
-
-import lombok.Data;
-
-@Data
-public class GetDatasetDateLineRequest {
-    private String startTime;
-    private String endTime;
-}
