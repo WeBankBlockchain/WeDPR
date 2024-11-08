b140dd4323ae8a71905e9c730debf039beaf4e2c
fqliao <fqliao@yeah.net>
2024-11-08 18:21:11 +0800

    remove wedpr admin (#129)
    Co-authored-by: caryliao(廖飞强) <caryliao@webank.com>

--- wedpr-components/admin/src/main/java/com/webank/wedpr/components/admin/response/CreateOrUpdateWedprAgencyResponse.java
+++
@@ -1,9 +0,0 @@
-package com.webank.wedpr.components.admin.response;
-
-import lombok.Data;
-
-/** Created by caryliao on 2024/8/22 23:23 */
-@Data
-public class CreateOrUpdateWedprAgencyResponse {
-    private String agencyId;
-}
--- wedpr-components/admin/src/main/java/com/webank/wedpr/components/admin/service/LocalShellService.java
+++
@@ -1,6 +0,0 @@
-package com.webank.wedpr.components.admin.service;
-
-/** Created by caryliao on 2024/8/24 20:16 */
-public interface LocalShellService {
-    boolean buildAuthorityCsrToCrt(String agencyName, String csrPath, long days);
-}
